61f965a895496b335e3b6f3aa01f37faa8720ef4
yuri <yuri_resende@hotmail.com>
2022-01-11 16:45:07 -0300

    Migration, JSON serialization, content negotiation

---
+++ 07 RestSpringBootUdemy Migrations/src/main/java/br/com/resende/controller/PersonController.java
@@ -0,0 +1,102 @@
+package br.com.resende.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.resende.data.vo.v1.PersonVO;
+import br.com.resende.services.PersonServices;
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+
+	@Autowired
+	private PersonServices services;
+	
+	@GetMapping(produces = {"application/json", "application/xml", "application/x-yaml"})
+	public List<PersonVO> findAll() {
+	
+		return services.findAll();
+	}
+	
+	@GetMapping(value = "/{id}", produces = {"application/json", "application/xml", "application/x-yaml"})
+	public PersonVO findByID(@PathVariable("id") Long id) {
+		
+		return services.findByID(id);
+	}
+	
+	
+	@PostMapping(produces = {"application/json", "application/xml", "application/x-yaml"}, 
+				consumes= {"application/json", "application/xml", "application/x-yaml"})
+	public PersonVO create(@RequestBody PersonVO person) {
+
+		return services.create(person);
+	}
+	
+	@PutMapping(produces = {"application/json", "application/xml", "application/x-yaml"},
+				consumes = {"application/json", "application/xml", "application/x-yaml"})
+	public PersonVO update(@RequestBody PersonVO person) {
+
+		return services.update(person);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {//retorna apenas status 200 ok no postman
+	
+		services.delete(id);
+		return ResponseEntity.ok().build();
+	}
+
+}
+
+
+/* Forma mais mais explicita de declarar os metodos do controller 
+@RequestMapping(method=RequestMethod.GET, 
+					produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+	
+		return services.findAll();
+	}
+	
+	@RequestMapping(value="/{id}", 
+					method=RequestMethod.GET, 
+					produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findByID(@PathVariable("id") Long id) {
+		
+		return services.findByID(id);
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+
+		return services.create(person);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+
+		return services.create(person);
+	}
+	
+	@RequestMapping(value="/{id}", 
+			method=RequestMethod.DELETE)
+	public void delete(@PathVariable("id") Long id) {//retorna apenas status 200 ok no postman
+	
+		services.delete(id);
+	}
+	*/
---
+++ 07 RestSpringBootUdemy Migrations/src/main/java/br/com/resende/serialization/converter/YamlJackson2HttpMessageConverter.java
@@ -0,0 +1,19 @@
+package br.com.resende.serialization.converter;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+
+
+public class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter{
+
+	public YamlJackson2HttpMessageConverter() {
+		super(new YAMLMapper(), MediaType.parseMediaType("application/x-yaml"));
+		
+	}
+	
+	
+
+}
